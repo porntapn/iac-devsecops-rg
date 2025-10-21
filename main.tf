0e01517eb740d750d5e7ba25dbf1b6a3fb0d1434
porntapn <admin@ais.co.th>
2025-10-21 08:51:15 +0000

    Initial Commit : Azure Resource Group and Insecure NSG

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_network_security_group" "example_nsg" {
     name                 = "iac-devops-nsg"
-    location             = azurerm_resource_group.rg
+    location             = azurerm_resource_group.rg.location
     resource_group_name  = azurerm_resource_group.rg.name
 }
 
